aa18c87852d4d605e5aa355b124a21dd89609365
Batuhan <batuhan.budak@gmail.com>
2022-10-31 14:16:24 +0300

    Add types for styled components

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -1,22 +1,37 @@
-import { TestimonialCardIndividual, TestimonialCardDesc, TestimonialCardJob, TestimonialCardName, TestimonialPersonalContainer, AvatarContainer, Avatar } from '../styles/TestimonialsStyles';
+import { StaticImageData } from "next/image";
+import {
+  TestimonialCardIndividual,
+  TestimonialCardDesc,
+  TestimonialCardJob,
+  TestimonialCardName,
+  TestimonialPersonalContainer,
+  AvatarContainer,
+  Avatar,
+} from "../styles/TestimonialsStyles";
 
-export default function TestimonialCard({ descText, name, job, avatar }) {
-    return (
-        <TestimonialCardIndividual>
-            <AvatarContainer>
-                <Avatar src={avatar} alt='avatar' />
-            </AvatarContainer>
-            <TestimonialCardDesc>
-                {descText}
-            </TestimonialCardDesc>
-            <TestimonialPersonalContainer>
-                <TestimonialCardName>
-                    {name}
-                </TestimonialCardName>
-                <TestimonialCardJob>
-                    {job}
-                </TestimonialCardJob>
-            </TestimonialPersonalContainer>
-        </TestimonialCardIndividual>
-    )
+interface Props {
+  descText: string;
+  name: string;
+  job: string;
+  avatar: StaticImageData;
+}
+
+export default function TestimonialCard({
+  descText,
+  name,
+  job,
+  avatar,
+}: Props) {
+  return (
+    <TestimonialCardIndividual>
+      <AvatarContainer>
+        <Avatar src={avatar} alt="avatar" />
+      </AvatarContainer>
+      <TestimonialCardDesc>{descText}</TestimonialCardDesc>
+      <TestimonialPersonalContainer>
+        <TestimonialCardName>{name}</TestimonialCardName>
+        <TestimonialCardJob>{job}</TestimonialCardJob>
+      </TestimonialPersonalContainer>
+    </TestimonialCardIndividual>
+  );
 }
