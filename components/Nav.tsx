f6073462c0d0b7e9a7997d13382105c32cfd4dad
Batuhan <batuhan.budak@gmail.com>
2022-10-31 14:08:42 +0300

    Add types for styled components

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,40 +1,50 @@
 import React from "react";
-import { NavBar, NavList, NavLink, NavItem, NavButton } from '../styles/NavBarStyles';
+import {
+  NavBar,
+  NavList,
+  NavLink,
+  NavItem,
+  NavButton,
+} from "../styles/NavBarStyles";
 
-export default function Nav({ closeNav, navOpen }) {
-    return (
-        <NavBar navOpen={navOpen}  >
-            <NavList navOpen={navOpen} id="primary-navigation">
-                <NavItem >
-                    <NavLink href="/#home" onClick={() => closeNav()}>
-                        Overview
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink href="/#services" onClick={() => closeNav()}>
-                        Examples
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink href="/#about" onClick={() => closeNav()}>
-                        Tour
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/#work" onClick={() => closeNav()}>
-                        Blog
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/#work" onClick={() => closeNav()}>
-                        Help
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavButton>Download</NavButton>
+interface Props {
+  closeNav: () => void;
+  navOpen: boolean;
+}
 
-                </NavItem>
-            </NavList>
-        </NavBar >
-    );
+export default function Nav({ closeNav, navOpen }: Props) {
+  return (
+    <NavBar navOpen={navOpen}>
+      <NavList navOpen={navOpen} id="primary-navigation">
+        <NavItem>
+          <NavLink href="/#home" onClick={() => closeNav()}>
+            Overview
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/#services" onClick={() => closeNav()}>
+            Examples
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/#about" onClick={() => closeNav()}>
+            Tour
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/#work" onClick={() => closeNav()}>
+            Blog
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/#work" onClick={() => closeNav()}>
+            Help
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavButton>Download</NavButton>
+        </NavItem>
+      </NavList>
+    </NavBar>
+  );
 }
